eb0e8a9046f6dbfc20c2ab8609424e40cd652db6
Nick Ruest <ruestn@gmail.com>
2017-07-31 13:51:10 -0400

    Setup mycila plugin, and normalize all license headers; Resolves #4. (#20)
    Travis-CI had one check fail during GitHub outage, others cleared. Overriding defaults.

--- src/main/scala/io/archivesunleashed/spark/matchbox/ExtractTextFromPDFs.scala
+++ src/main/scala/io/archivesunleashed/spark/matchbox/ExtractTextFromPDFs.scala
@@ -1,5 +1,6 @@
 /*
- * Warcbase: an open-source platform for managing web archives
+ * Archives Unleashed Toolkit (AUT):
+ * An open-source platform for analyzing web archives.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,12 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.archivesunleashed.spark.matchbox
 
 import java.io.ByteArrayInputStream
 
-//import org.apache.pig.data.DataByteArray
 import org.apache.tika.metadata.Metadata
 import org.apache.tika.parser.ParseContext
 import org.apache.tika.parser.pdf.PDFParser
@@ -26,25 +25,4 @@ import org.apache.tika.sax.BodyContentHandler;
 
 object ExtractTextFromPDFs {
   val pdfParser = new PDFParser()
-
-/*
-  def apply(dba: DataByteArray): String = {
-    if (dba.get.isEmpty) "N/A"
-    else {
-      try {
-        val is = new ByteArrayInputStream(dba.get)
-        val contenthandler = new BodyContentHandler(Integer.MAX_VALUE)
-        val metadata = new Metadata()
-        pdfParser.parse(is, contenthandler, metadata, new ParseContext())
-        is.close()
-        contenthandler.toString
-      }
-      catch {
-        case t: Throwable =>
-          t.printStackTrace()
-          ""
-      }
-    }
-  }
-*/
 }
