8ed9e574cda61a7f096ac03890f7a6094c1380f5
greebie <ryan.deschamps@gmail.com>
2017-12-08 08:39:56 -0500

    - Include test for flatten.default - Remove duplicate call to FunSuite in ExtractPopularImagesTest

--- src/test/scala/io/archivesunleashed/spark/matchbox/TupleFormatterTest.scala
+++ src/test/scala/io/archivesunleashed/spark/matchbox/TupleFormatterTest.scala
@@ -36,6 +36,7 @@ class TupleFormatterTest extends FunSuite {
     val tuple = ("a", 1, "c", ("x", 3, ("NO", "YES")), "perhaps", "maybe", 3, (0,1))
     assert(TupleFormatter.flatten(tuple) == ("a", 1, "c", "x", 3, "NO", "YES", "perhaps", "maybe", 3, 0, 1))
     assert(TupleFormatter.flatten.isInstanceOf[TupleFormatter.LowPriorityFlatten])
+    TupleFormatter.flatten.default shouldBe a [Poly1$CaseBuilder$$anon$1]
   }
 
   test ("Object extensions") {
