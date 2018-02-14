a6351c01c92505d42b261073bef583d2d9cb2f2e
Ryan Deschamps <ryan.deschamps@gmail.com>
2018-02-13 19:27:56 -0500

    Graphml Improvements (#170)
    * Remove references to Arc and WarcRecord libraries (covered by ArchiveRecord).
    Change references to GenericArchiveRecords to ArchiveRecords (meaning both Warc and Arc formats).
    Remove related tests.
    Change references to Generics in RecordLoader and tests.
    
    * Remove NER test (currently not working).
    
    * Remove GDF; Fix typos; Remove resolved references to deprecations.
    
    * Fix double space and typos.
    
    * More typos.
    
    * Capitalization fixes.
    
    * Fix comment out.
    
    * Restore ExtractEntities to master.
    
    * Fix graphml output.
    
    * Graphml changes:
    - remove dodgy pretty print attempt.
    - hash url to get a unique id.
    - restore labelling functionality
    
    * Fix problems with merge.

--- src/main/scala/io/archivesunleashed/spark/matchbox/WriteGraphML.scala
+++ src/main/scala/io/archivesunleashed/spark/matchbox/WriteGraphML.scala
@@ -22,6 +22,7 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Paths
 import org.apache.spark.rdd.RDD
+import java.security.MessageDigest
 
 /**
   * UDF for exporting an RDD representing a collection of links to a GDF file.
@@ -34,39 +35,47 @@ object WriteGraphML {
   *
   * Writes graph nodes and edges to file.
   */
+
+
+// Compute a hash of a file
+// The output of this function should match the output of running "md5 -q <file>"
+
   def apply(rdd: RDD[((String, String, String), Int)], graphmlPath: String): Boolean = {
     if (graphmlPath.isEmpty()) false
     else makeFile (rdd, graphmlPath)
   }
 
+  def computeHash(id: String): String = {
+    val md5 = MessageDigest.getInstance("MD5")
+    return md5.digest(id.getBytes).map("%02x".format(_)).mkString
+  }
+
   def makeFile (rdd: RDD[((String, String, String), Int)], graphmlPath: String): Boolean = {
     val outFile = Files.newBufferedWriter(Paths.get(graphmlPath), StandardCharsets.UTF_8)
-    val edges = rdd.map(r => "      <edge source=\"" + r._1._2 + "\" target=\"" +
-      r._1._3 + """"  type="directed">
-      <data key="WT">""" + r._2 + """</data>
-      <data key="CD">"""" + r._1._1 + """"</data>
-      </edge>""").collect
-    val nodes = rdd.flatMap(r => List("""
-             <node id="""" + r._1._2 + """">
-             </node>""",
-      "      <node id=\"" + r._1._3 + """">
-             </node>""")).distinct.collect
-    outFile.write("""<?xml version="1.0" encoding="UTF-8"?>
-      <graphml xmlns="http://graphml.graphdrawing.org/xmlns"
-               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-               xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
-               http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
-        <key id="WT" for="edge" attr.name="weight" attr.type="double">
-          <default>0.0</default>
-        </key>
-        <key id="CD" for="edge" attr.name="crawlDate" attr.type="string">
-        </key>
-        <graph mode="static" edgedefault="directed">
-          """)
-    nodes.foreach(r => outFile.write(r + "\n"))
-    edges.foreach(r => outFile.write(r + "\n"))
-    outFile.write("""</graph>
-      </graphml>""")
+    val edges = rdd.map(r => "<edge source=\"" + computeHash(r._1._2) + "\" target=\"" +
+      computeHash(r._1._3) + "\"  type=\"directed\">\n" +
+    "<data key=\"weight\">" + r._2 + "</data>\n" +
+    "<data key=\"crawlDate\">" + r._1._1 + "</data>\n" +
+    "</edge>\n").collect
+    val nodes = rdd.flatMap(r => List("<node id=\"" + computeHash(r._1._2) + "\">\n" +
+      "<data key=\"label\">" + r._1._2 + "</data>\n</node>\n",
+      "<node id=\"" + computeHash(r._1._3) + "\">\n" +
+      "<data key=\"label\">" + r._1._3 + "</data>\n</node>\n")).distinct.collect
+    outFile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+      "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"\n" +
+      "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+      "  xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns\n" +
+      "  http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\"\n>" +
+      "<key id=\"label\" for=\"node\" attr.name=\"label\" attr.type=\"string\" />\n" +
+      "<key id=\"weight\" for=\"edge\" attr.name=\"weight\" attr.type=\"double\">\n" +
+      "<default>0.0</default>\n" +
+      "</key>\n" +
+      "<key id=\"crawlDate\" for=\"edge\" attr.name=\"crawlDate\" attr.type=\"string\" />\n" +
+      "<graph mode=\"static\" edgedefault=\"directed\">\n")
+    nodes.foreach(r => outFile.write(r))
+    edges.foreach(r => outFile.write(r))
+    outFile.write("</graph>\n" +
+    "</graphml>")
     outFile.close()
     return true
   }
