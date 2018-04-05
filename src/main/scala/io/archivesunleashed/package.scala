a46ce3b76c1c97377ef60e57c379fc6f75ea7814
lintool <jimmylin@uwaterloo.ca>
2018-04-04 23:23:56 -0400

    CR

--- src/main/scala/io/archivesunleashed/package.scala
+++ src/main/scala/io/archivesunleashed/package.scala
@@ -1,3 +1,20 @@
+/*
+ * Archives Unleashed Toolkit (AUT):
+ * An open-source platform for analyzing web archives.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io
 
 import io.archivesunleashed.data.{ArchiveRecordWritable, ArchiveRecordInputFormat}
@@ -14,6 +31,9 @@ import org.json4s.jackson.JsonMethods._
 import scala.reflect.ClassTag
 import scala.util.matching.Regex
 
+/**
+  * Package object which supplies implicits to augment generic RDDs with AUT-specific transformations.
+  */
 package object archivesunleashed {
   object RecordLoader {
     def loadArchives(path: String, sc: SparkContext): RDD[ArchiveRecord] =
