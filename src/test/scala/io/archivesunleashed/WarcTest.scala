521d1e3b147098795f1f5a1e99f81c8a7f8891a1
Jimmy Lin <jimmylin@uwaterloo.ca>
2018-04-26 11:49:32 -0400

    Code refformatting. (#201)

--- src/test/scala/io/archivesunleashed/WarcTest.scala
+++ src/test/scala/io/archivesunleashed/WarcTest.scala
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.archivesunleashed
 
 import com.google.common.io.Resources
