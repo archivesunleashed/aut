edc830c62ecf4c8cc275af26ed9dbac677ecd07c
lintool <jimmylin@uwaterloo.ca>
2018-04-27 10:21:24 -0400

    Docs.

--- src/main/scala/io/archivesunleashed/df/package.scala
+++ src/main/scala/io/archivesunleashed/df/package.scala
@@ -3,9 +3,13 @@ package io.archivesunleashed
 import org.apache.spark.sql.functions.udf
 
 /**
-  * Created by jimmylin on 4/9/18.
+  * UDFs for data frames.
   */
 package object df {
+  // TODO: UDFs for use with data frames go here, tentatively. There are couple of ways we could build UDFs,
+  // by wrapping matchbox UDFs or by reimplementing them. The following examples illustrate. Obviously, we'll
+  // need to populate more UDFs over time, but this is a start.
+
   val ExtractDomain = udf(io.archivesunleashed.matchbox.ExtractDomain.apply(_: String, ""))
 
   val RemovePrefixWWW = udf[String, String](_.replaceAll("^\\s*www\\.", ""))
