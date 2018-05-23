7898b835c2652d8fcf99a6047d8ffd2a2454cefd
Joseph Zhou <wuhan.joseph.zhou@gmail.com>
2018-05-22 22:25:26 -0400

    Fix spacing

--- src/main/scala/io/archivesunleashed/matchbox/SaveImages.scala
+++ src/main/scala/io/archivesunleashed/matchbox/SaveImages.scala
@@ -26,40 +26,40 @@ import java.util.Base64
 /** Save images to disk from a dataframe */
 object SaveImages {
 
-	/** 
-	 * Given a dataframe, serializes the images and saves to disk
-	 * @param df the input dataframe
-	 * @param bytesColumnName the name of the column containing the image bytes
-	 * @param fileName the name of the file to save the images to (without extension)
-	 * e.g. fileName = "foo" => images are saved as foo0.jpg, foo1.jpg
-	*/
-	def apply(df: DataFrame, bytesColumnName: String, fileName: String) = {
-		var count = 0;
-		df.select(bytesColumnName).foreach(row => {
-			try {
-				// assumes the bytes are base64 encoded already as returned by ExtractImageDetails
-				val encodedBytes: String = row.getAs(bytesColumnName);
-				val bytes = Base64.getDecoder.decode(encodedBytes);
-				val in = new ByteArrayInputStream(bytes);
+  /** 
+   * Given a dataframe, serializes the images and saves to disk
+   * @param df the input dataframe
+   * @param bytesColumnName the name of the column containing the image bytes
+   * @param fileName the name of the file to save the images to (without extension)
+   * e.g. fileName = "foo" => images are saved as foo0.jpg, foo1.jpg
+  */
+  def apply(df: DataFrame, bytesColumnName: String, fileName: String) = {
+    var count = 0;
+    df.select(bytesColumnName).foreach(row => {
+      try {
+        // assumes the bytes are base64 encoded already as returned by ExtractImageDetails
+        val encodedBytes: String = row.getAs(bytesColumnName);
+        val bytes = Base64.getDecoder.decode(encodedBytes);
+        val in = new ByteArrayInputStream(bytes);
 
-				val input = ImageIO.createImageInputStream(in);
-				val readers = ImageIO.getImageReaders(input);
-				if (readers.hasNext()) {
-					val reader = readers.next()
-					reader.setInput(input)
-					val image = reader.read(0)
+        val input = ImageIO.createImageInputStream(in);
+        val readers = ImageIO.getImageReaders(input);
+        if (readers.hasNext()) {
+          val reader = readers.next()
+          reader.setInput(input)
+          val image = reader.read(0)
 
-					val format = reader.getFormatName()
-		      val file = new File(fileName + count + "." + format);
-		      count += 1
-		      if (image != null) {
-		      	ImageIO.write(image, format, file);
-		      }
-				}
-	      
-			} catch {
-				case e: Throwable => {}
-			}
-		})
-	}
+          val format = reader.getFormatName()
+          val file = new File(fileName + count + "." + format);
+          count += 1
+          if (image != null) {
+            ImageIO.write(image, format, file);
+          }
+        }
+        
+      } catch {
+        case e: Throwable => {}
+      }
+    })
+  }
 }
