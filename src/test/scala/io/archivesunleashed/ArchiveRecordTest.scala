7731b6de4f8367be3111f91c5c4960fbad7d3652
Ryan Deschamps <ryan.deschamps@gmail.com>
2018-11-27 19:15:41 -0500

    Add .getHttpStatus and .getArchiveFile to ArchiveRecordImpl class #198 & #164 (#292)
    * Resolves #198
    * Resolves #164
    * Add getHttpStatus to ArchiveRecord class & trait
      - add .getHttpStatus to potential outputs
      - add tests for .getHttpStatus calls
      - improve ArchiveRecord testing overall.
    * Add .getArchiveFile feature to ArchiveRecordImpl.
      - add getArchiveFile to trait
      - add getArchiveFile for ArchiveRecordImpl
      - add tests for getArchiveFile.
    * Other code style fixes.
    * Include updates to tests.

--- src/test/scala/io/archivesunleashed/ArchiveRecordTest.scala
+++ src/test/scala/io/archivesunleashed/ArchiveRecordTest.scala
@@ -22,6 +22,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfter, FunSuite}
+import org.apache.commons.io.FilenameUtils
 
 @RunWith(classOf[JUnitRunner])
 class ArchiveRecordTest extends FunSuite with BeforeAndAfter {
@@ -44,10 +45,68 @@ class ArchiveRecordTest extends FunSuite with BeforeAndAfter {
     assert(RecordLoader.loadArchives(warcPath, sc).count == 299L)
   }
 
+  test("Resource name produces expected result.") {
+    val textSampleArc = RecordLoader.loadArchives(arcPath, sc)
+     .map(x => FilenameUtils.getName(x.getArchiveFilename))
+     .take(3)
+    val textSampleWarc = RecordLoader.loadArchives(warcPath, sc)
+     .map(x => FilenameUtils.getName(x.getArchiveFilename)).take(3)
+    assert(textSampleArc.deep == Array("example.arc.gz",
+      "example.arc.gz", "example.arc.gz").deep)
+    assert(textSampleWarc.deep == Array("example.warc.gz",
+      "example.warc.gz", "example.warc.gz").deep)
+  }
+
+  test("Crawl Dates") {
+    val textSampleArc = RecordLoader.loadArchives(arcPath, sc)
+     .map(x => x.getCrawlDate).take(3)
+    val textSampleWarc = RecordLoader.loadArchives(warcPath, sc)
+     .map(x => x.getCrawlDate).take(3)
+    assert(textSampleArc.deep == Array("20080430", "20080430", "20080430").deep)
+    assert(textSampleWarc.deep == Array("20080430", "20080430", "20080430").deep)
+  }
+
+  test("Domains") {
+    val textSampleArc = RecordLoader.loadArchives(arcPath, sc)
+     .map(x => x.getDomain).take(3)
+    val textSampleWarc = RecordLoader.loadArchives(warcPath, sc)
+     .map(x => x.getDomain).take(3)
+    assert(textSampleArc.deep == Array("", "", "www.archive.org").deep)
+    assert(textSampleWarc.deep == Array("", "www.archive.org", "www.archive.org").deep)
+  }
+
+  test("Urls") {
+    val textSampleArc = RecordLoader.loadArchives(arcPath, sc)
+     .map(x => x.getUrl).take(3)
+    val textSampleWarc = RecordLoader.loadArchives(warcPath, sc)
+     .map(x => x.getUrl).take(3)
+    assert(textSampleArc.deep == Array("filedesc://IAH-20080430204825-00000-blackbook.arc",
+      "dns:www.archive.org", "http://www.archive.org/robots.txt").deep)
+    assert(textSampleWarc.deep == Array("dns:www.archive.org",
+      "http://www.archive.org/robots.txt", "http://www.archive.org/").deep)
+  }
+
+  test("Mime-Type") {
+    val textSampleArc = RecordLoader.loadArchives(arcPath, sc)
+      .map(x => x.getMimeType).take(3)
+    val textSampleWarc = RecordLoader.loadArchives(warcPath, sc)
+      .map(x => x.getMimeType).take(3)
+    assert (textSampleArc.deep == Array ("text/plain", "text/dns", "text/plain").deep)
+    assert (textSampleWarc.deep == Array("unknown", "text/plain", "text/html").deep)
+  }
+
+  test("Get Http Status") {
+    val textSampleArc = RecordLoader.loadArchives(arcPath, sc)
+      .map(x => x.getHttpStatus).take(3)
+    val textSampleWarc = RecordLoader.loadArchives(warcPath, sc)
+      .map(x => x.getHttpStatus).take(3)
+    assert (textSampleArc.deep == Array("000", "000", "200").deep)
+    assert (textSampleWarc.deep == Array("000", "200", "200").deep)
+  }
+
   after {
     if (sc != null) {
       sc.stop()
     }
   }
 }
-
