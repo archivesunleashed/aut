7a61f0e2201fd9e316af0b257dc3dcfd76ea7e25
Hsiu-Wei <yhsiuwei@gmail.com>
2019-07-05 05:59:57 -0400

    Add image analysis and extraction w/TensorFlow (#318)

---
+++ src/main/python/tf/extract_images.py
@@ -0,0 +1,17 @@
+import numpy as np
+import argparse
+from model.object_detection import SSDExtractor
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Extracting images from model output.')
+    parser.add_argument('--res_dir', help='Path of result (model output) directory.')
+    parser.add_argument('--output_dir', help='Path of extracted image file output directory.')
+    parser.add_argument('--threshold', type=float, help='Threshold of detection confidence scores.')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+    extractor = SSDExtractor(args.res_dir, args.output_dir)
+    extractor.extract_and_save(class_ids="all", threshold=args.threshold)
