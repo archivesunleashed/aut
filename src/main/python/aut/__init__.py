5255c10e64ddfbd530cb08fda468e79cbdb81b67
nruest <ruestn@gmail.com>
2020-01-23 08:50:51 -0500

    Start adding filters; keep_valid_pages.
    - TODO, make it object oriented

--- src/main/python/aut/__init__.py
+++ src/main/python/aut/__init__.py
@@ -1,8 +1,8 @@
 from aut.common import WebArchive
+from aut.filters import keep_valid_pages
 from aut.udfs import (
     compute_MD5,
     compute_SHA1,
-    detect_language,
     extract_domain,
     remove_html,
     remove_http_headers,
@@ -13,8 +13,8 @@ __all__ = [
     "WebArchive",
     "compute_MD5",
     "compute_SHA1",
-    "detect_language",
     "extract_domain",
+    "keep_valid_pages",
     "remove_html",
     "remove_prefix_www",
     "remove_http_headers",
---
+++ src/main/python/aut/filters.py
@@ -0,0 +1,24 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import array_contains, col, explode
+
+
+def __init__(self, sc, sqlContext, df):
+    self.sc = sc
+    self.sqlContext = sqlContext
+    self.df = df
+
+
+def keep_valid_pages(df):
+    return (
+        df.filter("crawl_date is not NULL")
+        .filter(
+            ~(col("url").rlike(".*robots\\.txt$"))
+            & (
+                col("mime_type_web_server").rlike("text/html")
+                | col("mime_type_web_server").rlike("application/xhtml+xml")
+                | col("url").rlike("(?i).*htm$")
+                | col("url").rlike("(?i).*html$")
+            )
+        )
+        .filter(col("http_status_code") == 200)
+    )
