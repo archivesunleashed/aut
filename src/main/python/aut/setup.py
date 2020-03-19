3eb9393566587b3c9c8a6d23d2a9732f5ec78b3c
nruest <ruestn@gmail.com>
2020-03-19 10:05:35 -0400

    Add setup; might not need it in the end :shrug:

---
+++ src/main/python/aut/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="aut",
+    version="0.18.1",
+    url="https://github.com/archivesunleashed/aut",
+    description="The Archives Unleashed Toolkit is an open-source toolkit for analyzing web archives.",
+    author="Nick Ruest",
+    author_email="ruestn@yorku.ca",
+    license="Apache 2.0",
+    packages=find_packages(),
+    zip_safe=False,
+)
