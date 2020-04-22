fbc6f727f065868739eb56d81b78751a16bf53a3
nruest <ruestn@gmail.com>
2020-04-22 15:04:31 -0400

    review

--- src/main/scala/io/archivesunleashed/app/PlainTextExtractor.scala
+++ src/main/scala/io/archivesunleashed/app/PlainTextExtractor.scala
@@ -17,8 +17,7 @@
 package io.archivesunleashed.app
 
 import io.archivesunleashed.ArchiveRecord
-import io.archivesunleashed.df.{ExtractBoilerpipeTextDF, RemoveHTMLDF,
-                                RemoveHTTPHeaderDF}
+import io.archivesunleashed.df.{ExtractBoilerpipeTextDF}
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 object PlainTextExtractor {
